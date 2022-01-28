aa26387033b7449049127b8aa4852aca12b9c5cc
Unknown <Artymediys@yandex.ru>
2022-01-28 14:17:06 +0300

    added TIP3 sup and changed getTokensBack func

--- Airdrop.sol
+++ Airdrop.sol
@@ -1,9 +1,11 @@
-pragma ton-solidity >= 0.35.0;
+pragma ton-solidity >= 0.39.0;
 pragma AbiHeader time;
 pragma AbiHeader expire;
 pragma AbiHeader pubkey;
 
-import "IRootTokenContract.sol";
+import "Interfaces/IRootTokenContract.sol";
+import "Interfaces/ITONTokenWallet.sol";
+import "Libraries/MsgFlag.sol";
 
 // part of main Airdrop SC (edit what you need)
 contract Airdrop {
@@ -11,9 +13,12 @@ contract Airdrop {
     address token_wallet;
 
     // example value
-    uint128 deploy_wallet_grams = 0.2 ton;
+    uint128 constant deploy_wallet_grams = 0.2 ton;
+    uint128 constant transfer_grams = 0.5 ton;
 
-    mapping(address => uint128) users;
+    mapping(address => uint128) receivers;
+
+    uint128 transferred_count = 0;
 
     constructor(address _token) public {
         require(tvm.pubkey() != 0, 101);
@@ -26,10 +31,18 @@ contract Airdrop {
 
     function setUpTokenWallet() internal view {
         // Deploy token wallet
-        IRootTokenContract(token).deployEmptyWallet(deploy_wallet_grams, 0, address(this), address(this));
+        IRootTokenContract(token).deployEmptyWallet{value: 1 ton}(
+            deploy_wallet_grams,
+            0,
+            address(this),
+            address(this)
+        );
 
         // Request for token wallet address
-        IRootTokenContract(token).getWalletAddress{callback: Airdrop.setTokenWalletAddress}(0, address(this));
+        IRootTokenContract(token).getWalletAddress{
+            value: 1 ton,
+            callback: Airdrop.setTokenWalletAddress
+        }(0, address(this));
     }
     
     function setTokenWalletAddress(address wallet) external {
@@ -37,25 +50,42 @@ contract Airdrop {
         token_wallet = wallet;
     }
 
-    // need to add everything we need here
+    // add every info we need here
     function getDetails() external view returns(
         address _token,
         address _token_wallet,
+        uint128 _transferred_count,
         address _airdrop_sc,
         uint128 _airdrop_sc_balance
     ) {
         return (
             token,
             token_wallet,
+            transferred_count,
             address(this),
             address(this).balance
         );
     }
 
     function getTokensBack(uint128 amount) external view {
-        require(users.exists(msg.sender) && amount <= users[msg.sender], 104);
+        require(receivers.exists(msg.sender) && amount <= receivers[msg.sender], 104);
         tvm.accept();
 
-        msg.sender.transfer(amount, true, 1);
+        TvmCell empty;
+
+        // Transfer tokens
+        ITONTokenWallet(token_wallet).transferToRecipient{
+            value: 0,
+            flag: MsgFlag.ALL_NOT_RESERVED
+        }(
+            0,
+            msg.sender,
+            amount,
+            deploy_wallet_grams,
+            transfer_grams,
+            msg.sender,
+            false,
+            empty
+        );
     }
 }
